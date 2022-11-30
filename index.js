73e971ac2e3eadb36034349d1179a3a93ddfb077
Poomathi <poomathi.k@kaaviansys.com>
2022-11-30 14:37:01 +0530

    Changing index.js file..

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require('express');
-const app = express();
-const fs = require('fs');
+// const app = express();
+// const fs = require('fs');
 const path = require('path');
 const NODE_ENV = process.env.NODE_ENV;
 
 // For any other request, serve HTML in DIT environment (cloud env)
-exports.build = function(directoryName, filePath){
+/**
+ * 
+ * @param {
+ * } app 
+ */
+exports.build = function(app){
   if (NODE_ENV === 'DIT') {
-    const indexHTMLContent = fs.readFileSync(
-      path.join(directoryName + filePath),
-      'utf8'
-    );
     app.all('*', (req, res) => {
-      res.send(indexHTMLContent);
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
   }
-}
-
-// For serving built static js/css files
-exports.builtStaticFiles = function(folderName, directoryName, filePath){
-  app.use(
-    folderName,
-    express.static(path.join(directoryName, filePath))
-  );
+  console.log(__dirname);
+  app.use(express.static(path.join(__dirname, 'build')));
 }
