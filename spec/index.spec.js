5aeae598377c113a4ee65a9ffc4c6ba473f2805c
Poomathi <poomathi.k@kaaviansys.com>
2022-12-05 10:14:03 +0530

    Changing variables and js doc..

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -6,10 +6,45 @@ describe('Server built files', () => {
   afterEach(async () => {
     sandbox.restore();
   });
-  it('Server Static folder', async () => {
+  it('Serve built htnl file', async () => {
     const bulidStub = sandbox.stub(buildFunction, 'build').returns(true);
     const mockResponse = { json: bulidStub };
     buildFunction.build(mockResponse);
     expect(mockResponse.json.getCall(0).returnValue).toEqual(true);
-    });
-})
+  });
+  it('Serve built htnl file', async () => {
+    const bulidStub = sandbox.stub(buildFunction, 'build').returns('Error..');
+    const mockResponse = { json: bulidStub };
+    buildFunction.build(mockResponse);
+    expect(mockResponse.json.getCall(0).returnValue).toEqual('Error..');
+  });
+  it('Serve built Static folder', async () => {
+    const bulidStub = sandbox.stub(buildFunction, 'builtStaticFiles').returns(true);
+    const mockResponse = { json: bulidStub };
+    buildFunction.build(mockResponse);
+    expect(mockResponse.json.getCall(0).returnValue).toEqual(true);
+  });
+
+  it('should initialise express server', () => {
+    sandbox.stub('express');
+    const mockResponse = () => {
+      return {
+        use: sandbox.callBack()
+      }
+    };
+    Object.defineProperty(mockResponse, 'static', { value: sandbox.callBack() })
+    return mockResponse;
+  });
+});
+
+  // jest.mock('express', () => {
+  //   const mockedExpress = () => {
+  //     return {
+  //       get: jest.fn(),
+  //       use: jest.fn(),
+  //       listen: jest.fn(),
+  //     }
+  //   };
+  //   Object.defineProperty(mockedExpress, "static", { value: jest.fn() });
+  //   return mockedExpress;
+  // });
