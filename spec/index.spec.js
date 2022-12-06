344247a6ac72a25baa291dd3d92865f0911d4af8
Poomathi <poomathi.k@kaaviansys.com>
2022-12-06 17:42:04 +0530

    Changing build and buildStaticFile Modules..

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,6 +1,8 @@
 const sinon = require('sinon');
 const sandbox = sinon.createSandbox();
 const buildFunction = require('../index');
+const request = require('request');
+const { application } = require('express');
 
 describe('Server built files', () => {
   afterEach(async () => {
@@ -14,29 +16,42 @@ describe('Server built files', () => {
   });
 
   it('Serve built html file', async () => {
-    const bulidStub = sandbox.stub(buildFunction, 'build').returns('Error..');
+    const bulidStub = sandbox.stub(buildFunction, 'build').returns('Something went wrong!');
     const mockResponse = { json: bulidStub };
     buildFunction.build(mockResponse);
-    expect(mockResponse.json.getCall(0).returnValue).toEqual('Error..');
+    expect(mockResponse.json.getCall(0).returnValue).toEqual('Something went wrong!');
   });
 
   it('Serve built html file', async () => {
-    const value = buildFunction.builtStaticFiles({ application: 'app', rootDirectory: __dirname, filePath: '/../client/build/index.html' });
+    const value = buildFunction.build({ application: 'app', rootDirectory: 'D:/JS/CHECK-IN/server', filePath: '/../client/build/index.html' });
+    sandbox.stub(application,'all');
     console.log(value);
     expect(value).toEqual(value);
   });
 
+  it('Static folder', async () => {
+    const value = buildFunction.build({application: 'app', rootDirectory: __dirname, filePath: '/../client/build/index.html' });
+    sandbox.stub('app',  () => {
+      const mockResponse = () => {
+        return {
+          use: sandbox.stub(value)
+        }
+      };
+      expect(mockResponse, 'static').toEqual(mockResponse)
+    });
+    // expect(value).toEqual(true);
+  });
+
   it('Serve built html file..', async () => {
-    let fs = require('fs');
-    const bulidStub = sandbox.stub(buildFunction, 'build').returns(true);
-    const readFileStub = await sinon.stub(fs,'readFileSync').returns();
-    console.log(readFileStub);
-    expect(bulidStub).toEqual(readFileStub);
+    sandbox.stub(buildFunction, 'build').returns(true);
+    const value = sinon.stub(fs,'readFileSync')
+    expect(value).toEqual(true);
   });
 });
 
 describe('Serve built files', () => {
   it('Static folder', async () => {
+    sandbox.spy(application,'use');
     const value = buildFunction.builtStaticFiles({application: 'app', folderName: '/static', rootDirectory: __dirname, filePath: '/../client/build/static' });
     sandbox.stub('app',  () => {
       const mockResponse = () => {
@@ -48,4 +63,24 @@ describe('Serve built files', () => {
     });
     // expect(value).toEqual(true);
   });
+  it('Static folder', async () => {
+    const value = buildFunction.builtStaticFiles({application: 'app', folderName: '/static', rootDirectory: __dirname, filePath: '/../client/build/static' });
+    sandbox.stub('app',  () => {
+      const mockResponse = () => {
+        return {
+          use: sandbox.stub(value),
+          res: 500
+        }
+      };
+      expect(mockResponse, 'static').toEqual(500);
+    });
+    // expect(value).toEqual(true);
+  });
+
+  it('Serve built html file', async () => {
+    const value = buildFunction.builtStaticFiles({ application: 'app', folderName: '/static', rootDirectory: __dirname, filePath: '/../client/build/static' });
+    console.log(value);
+    expect(value).toEqual(value);
+  });
+
 });
